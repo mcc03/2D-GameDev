6ea5ff28b651015e0a1f9a4584845a965ac4b5a2
Asiatisch <N00201490@iadt.ie>
2024-03-20 14:18:27 +0000

    Modifing and attempting to add a knockback system

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,6 +8,9 @@ public class EnemyMovement : MonoBehaviour
     EnemyStats enemy;
     Transform player;
 
+    Vector2 knockbackVelocity;
+    float knockbackDuration;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,25 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime); // always moving enemy to the player
+        // if we are currently knock back, then proceed the knockback
+        if(knockbackDuration > 0)
+        {
+            transform.position += (Vector3)knockbackVelocity * Time.deltaTime;
+            knockbackDuration -= Time.deltaTime;
+        }
+        else
+        {   // otherwise, cosntanly move the enemy towards the player
+             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime); // always moving enemy to the player
+        }
+       
+    }
+
+    // This is meant to be called from other scripts to create knockback
+    public void knockback( Vector2 velocity, float duration)
+    {
+        if(knockbackDuration > 0) return;
+
+        knockbackVelocity = velocity;
+        knockbackDuration = duration;
     }
 }
--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+
 public class EnemyStats : MonoBehaviour
 {
     //reference for enemy scriptable object
@@ -16,6 +18,17 @@ public class EnemyStats : MonoBehaviour
     [HideInInspector]
     public float currentDamage;
 
+    public float despawnDistance = 20f;
+    Transform player;
+
+    [Header("Damage Feedback")]
+    public Color damageColor = new Color(1,0,0,1);// what the color of the dmage flash should be.
+    public float damageFlashDuration = 0.2f; // how long the flash should last
+    public float deathFadeTime = 0.6f; // howw much time it takes for the enemy to fade.
+    Color originalColor;
+    SpriteRenderer sr;
+    EnemyMovement movement;
+
     void Awake()
     {
         currentMoveSpeed = enemyData.MoveSpeed;
@@ -23,10 +36,36 @@ public class EnemyStats : MonoBehaviour
         currentDamage = enemyData.Damage;
     }
 
-    //allows us to damage the enemy
-    public void TakeDamage(float dmg)
+    void Start()
+    {
+        player = FindObjectOfType<PlayerStats>().transform;
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+        movement = GetComponent<EnemyMovement>();
+    }
+
+   /*  void update()
+    {
+        if(Vector2.Distance(transform.position, player.position) >= despawnDistance)
+        {
+
+        }
+    } */
+
+    //This function always needs at least 2 values, the amount dealth <dmg> as welll as where the damage us
+    // coming from. whoch is passed as <sourePostion>. The <sourcePosition> is necessary because it is used to calculate 
+    // the direction of the knockback
+    public void TakeDamage(float dmg, Vector2 sourcePosition,float knockbackForce = 5f, float knockbackDuration = 0.2f)
     {
         currentHealth -= dmg;
+        StartCoroutine(DamageFlash());
+         // apply knockback if it is not zero
+        if(knockbackForce > 0)
+        {
+            // gets the direction of knockbacks
+            Vector2 dir = (Vector2)transform.position - sourcePosition;
+            movement.knockback(dir.normalized * knockbackForce, knockbackDuration);
+        }
 
         if(currentHealth <= 0)
         {
@@ -34,8 +73,35 @@ public class EnemyStats : MonoBehaviour
         }
     }
 
+    // This is a Coroutine function that makes the enemy flash when taking damage.
+    IEnumerator DamageFlash()
+    {
+        sr.color = damageColor;
+        yield return new WaitForSeconds(damageFlashDuration);
+        sr.color = originalColor;
+    }
+
     public void Kill()
     {
+        StartCoroutine(KillFade());
+       // Destroy(gameObject);
+    }
+
+    IEnumerator KillFade()
+    {
+        //waits for a single frame
+        WaitForEndOfFrame w = new WaitForEndOfFrame();
+        float t = 0, origaAlpha = sr.color.a;
+
+        // This is a loop that fires every frame.
+        while(t < deathFadeTime) {
+            yield return w;
+            t += Time.deltaTime;
+
+            //set the color for theis frame 
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (1- t/ deathFadeTime) + origaAlpha);
+        }
+
         Destroy(gameObject);
     }
 
